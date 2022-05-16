80744d28a4cae143a744aea4d5a401e4fd87c043
Owen <lebec.owen@yahoo.fr>
2022-05-16 12:38:09 +0200

    fix front-end

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
 import { CircularProgress } from "@material-ui/core";
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   const email = useRef();
@@ -21,9 +22,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">🍑 Fessebook</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Fessbook, partager vos fesses avec vos amis           
           </span>
         </div>
         <div className="loginRight">
@@ -51,13 +52,13 @@ export default function Login() {
               )}
             </button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
+            <Link to="/" className="loginRegisterButton">
               {isFetching ? (
                 <CircularProgress color="white" size="20px" />
               ) : (
                 "Create a New Account"
               )}
-            </button>
+            </Link>
           </form>
         </div>
       </div>
