fdc3ae11a45822dbabfd21ef3bf9ea4ae46da4fe
Mathieu <76435687+MathieuPicart@users.noreply.github.com>
2022-05-23 17:15:10 +0200

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var User = require('./../model/User')
-
-router.post('/', function(req, res, next) {
-  res.send({ msg : "Hello!" });
-});
-
-module.exports = router;
